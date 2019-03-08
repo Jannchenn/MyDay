5990eb44f581dd1e1812a52c27d3942b25bea4a8
jannchenn <chenanyi1997@yahoo.com>
2019-03-08 14:28:37 -0800

    add personal info summary

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions } from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import Welcome from '../screens/DailySummaryComponent';
+import { View, Text, StyleSheet, Card } from 'react-native';
 import AgendaView from '../react-native-calendars-master/src/agenda/index'
+import { db } from '../config'
 
 // let thoughtRef = db.ref('/thought');
 // let tagsRef = db.ref('/tag');
+let infoRef = db.ref('/person/info');
 
 var d = "";
 
@@ -18,24 +18,104 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.d = "";
-        this.state = {};
-        this.onDayPress = this.onDayPress.bind(this);
+        this.state = {items: {},info:{}};
     }
+
+    componentDidMount() {
+        infoRef.on('value', snapshot => {
+          let data = snapshot.val();
+          this.state.info["name"] = data.Name;
+          this.state.info["age"] = data.Age;
+          this.state.info["fp"] = data.Food_Preference;
+          this.state.info["fr"] = data.Food_Restriction;
+          this.state.info["gender"] = data.Gender;
+          this.state.info["height"] = data.Height;
+          this.state.info["weight"] = data.Weight;
+        });
+      }
 
     render() {
         return(
                 <AgendaView
-                    
+                    items={this.state.items}
+                    loadItemsForMonth={this.loadItems.bind(this)}
+                    renderItem={this.renderItem.bind(this)}
+                    renderEmptyDate={this.renderEmptyDate.bind(this)}
+                    rowHasChanged={this.rowHasChanged.bind(this)}
                 />
                 
         );
     }
-    onDayPress(day) {
-        this.setState({
-          selected: day.dateString
-        });
+    loadItems(day) {
+        setTimeout(() => {
+          for (let i = -15; i < 85; i++) {
+            const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+            const strTime = this.timeToString(time);
+            if (!this.state.items[strTime]) {
+              this.state.items[strTime] = [];
+            //   const numItems = Math.floor(Math.random() * 5);
+            //   for (let j = 0; j < numItems; j++) {
+            //     this.state.items[strTime].push({
+            //       name: 'Item for ' + strTime,
+            //       height: Math.max(50, Math.floor(Math.random() * 150))
+            //     });
+            //   }
+            }
+          }
+          //console.log(this.state.items);
+          const newItems = {};
+          Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+          this.setState({
+            items: newItems
+          });
+        }, 1000);
+        // console.log(`Load Items for ${day.year}-${day.month}`);
       }
+    
+      renderItem(item) {
+        return (
+          <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+        );
+      }
+    
+      renderEmptyDate() {
+        return (
+          <View style={styles.emptyDate}>
+                <Text><Text style={{fontWeight: "bold"}}> Name: </Text>{this.state.info.name}<Text style={{fontWeight: "bold"}}> Age: </Text>{this.state.info.age}</Text>
+                <Text><Text style={{fontWeight: "bold"}}> Gender: </Text>{this.state.info.gender}</Text>
+                <Text><Text style={{fontWeight: "bold"}}> Height: </Text>{this.state.info.height}<Text style={{fontWeight: "bold"}}> Weight: </Text>{this.state.info.weight}</Text>
+                <Text><Text style={{fontWeight: "bold"}}> Food Preference: </Text>{this.state.info.fp}</Text>
+                <Text><Text style={{fontWeight: "bold"}}> Food Restriction: </Text>{this.state.info.fr}</Text>
+          </View>
+        );
+      }
+    
+      rowHasChanged(r1, r2) {
+        return r1.name !== r2.name;
+      }
+    
+      timeToString(time) {
+        const date = new Date(time);
+        return date.toISOString().split('T')[0];
+      }
+   
 }
+
+const styles = StyleSheet.create({
+    item: {
+      backgroundColor: 'white',
+      flex: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      marginTop: 17
+    },
+    emptyDate: {
+      height: 120,
+      flex:1,
+      paddingTop: 30
+    }
+  });
 
 
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -213,36 +213,3 @@ export default MainNavigator = createDrawerNavigator({
 }, {
   drawerBackgroundColor: '#D1C4E9'
 });
-
-const styles = StyleSheet.create({  
-	container: {
-		flex: 1,
-		width: '100%',
-		height: '100%',
-  },
-  overlayContainer: {
-  	flex:1,
-  	backgroundColor: 'rgba(47,163,218, .3)'
-  },
-  top: {
-  	height:'60%',
-  	alignItems: 'center',
-  	justifyContent: 'center',
-  },
-  header: {
-  	color: '#fff',
-  	fontSize: 30,
-  	borderColor: '#fff',
-  	borderWidth: 2,
-  	padding: 20, 
-  	paddingLeft: 40,
-  	paddingRight: 40,
-  	backgroundColor: 'rgba(255,255,255, .1)',
-  },
-  menuContainer: {
-  	height: '40%',
-  	flexDirection: 'row',
-    flexWrap: 'wrap',
-  	justifyContent: 'center'
-  }
-});
